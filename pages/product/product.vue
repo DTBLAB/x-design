ddb62fbb5c0f6ed33141891a3a761633a0bb8adb
Starink <161250119@qq.com>
2020-09-19 14:32:04 +0800

    remove local images

--- pages/product/product.vue
+++ pages/product/product.vue
@@ -1,7 +1,7 @@
 <template>
 	<view class="product-container">
 		<view class="product-background">
-			<image class="product-image" src="/static/image/product/product.png" mode="widthFix"></image>
+			<image class="product-image" src="https://x-design.oss-cn-hangzhou.aliyuncs.com/product/product.png" mode="widthFix"></image>
 		</view>
 			
 <!-- 		<view class="prodcut-bannercontainer">
@@ -14,11 +14,11 @@
 		
 		<view class="product-bottom">
 			<navigator class="product-bottom__home" url="../index/index">
-				<image class="product-bottom__home__icon" src="/static/image/product/首页.png" mode="widthFix"></image>
+				<image class="product-bottom__home__icon" src="https://x-design.oss-cn-hangzhou.aliyuncs.com/product/首页.png" mode="widthFix"></image>
 				<text class="product-bottom__home__text">首页</text>
 			</navigator>
 			<navigator class="product-bottom__service">
-				<image class="product-bottom__service__icon" src="/static/image/product/客服.png" mode="widthFix"></image>
+				<image class="product-bottom__service__icon" src="https://x-design.oss-cn-hangzhou.aliyuncs.com/product/客服.png" mode="widthFix"></image>
 				<text class="product-bottom__service__text">客服</text>
 			</navigator>
 			<button class="product-bottom__button1" type="default">加入购物车</button>
@@ -38,13 +38,13 @@
 				活动
 				<view class="product-marketing__more">
 				{{sales}}
-				<image src="/../../static/image/home/路径 2018@3x.png"  class="product-marketing__more__icon"></image>
+				<image src="/../../static/image/more@3x.png"  class="product-marketing__more__icon"></image>
 				</view>
 			</navigator>
 			<view class="product-marketing__name">发货
 				<view class="product-marketing__place">
 					{{expressAddress}}
-					<image src="/../../static/image/product/直线139@3x.png"  class="product-marketing__place__icon"></image>
+					<image src="https://x-design.oss-cn-hangzhou.aliyuncs.com/product/直线139@3x.png"  class="product-marketing__place__icon"></image>
 					快递：￥{{expressFee.toFixed(2)}}
 				</view>
 			 </view>
@@ -55,13 +55,13 @@
 			<!-- 这一页的商品评价部分作为商品评价页的链接 只显示第一个 -->
 			<navigator class="product-comment__list" v-for="(item, index) in commentList" :key="index" url="...">
 							<view class="product-comment__item">
-								<image src="/../../static/image/product/椭圆411@3x.png"  class="product-comment__item__left"></image>
+								<image src="https://x-design.oss-cn-hangzhou.aliyuncs.com/product/椭圆411@3x.png"  class="product-comment__item__left"></image>
 								<view class="product-comment__item__right">
 									<view class="product-comment__item__title">
 										<view>{{item.username}}</view>
 											<view class="product-comment__item__stars">
-											<image v-for="count in item.scores" class="product-comment__item__starIcon" src="/../../static/image/product/星星黄色.png" mode=""></image>
-											<image v-for="count in 5-item.scores" class="product-comment__item__starIcon" src="/../../static/image/product/星星灰色.png" mode=""></image>
+											<image v-for="count in item.scores" class="product-comment__item__starIcon" src="https://x-design.oss-cn-hangzhou.aliyuncs.com/product/星星黄色.png" mode=""></image>
+											<image v-for="count in 5-item.scores" class="product-comment__item__starIcon" src="https://x-design.oss-cn-hangzhou.aliyuncs.com/product/星星灰色.png" mode=""></image>
 											</view>
 									</view>
 									<view>{{item.content}}</view>
@@ -100,29 +100,32 @@
 	export default {
 		components: {tabBar},
 		data() {
-		return {
-			productName:"帆布包",
-			productPrize:29.00,
-			productSold:100,
-			sales:"首次下单立减10元",
-			expressAddress:"浙江杭州",
-			expressFee:6,
-			productImages:"/static/image/product/product.png",
+			return {
+				productName:"帆布包",
+				productPrize:29.00,
+				productSold:100,
+				sales:"首次下单立减10元",
+				expressAddress:"浙江杭州",
+				expressFee:6,
+				productImages:"https://x-design.oss-cn-hangzhou.aliyuncs.com/product/product.png"
+			}
+		}
+	}
 			current: 0,
 			productinfoImages:[
-				{img: '/static/image/product/info.jpg', name: '', url:''},
-				{img: '/static/image/product/info.jpg', name: '', url:''},
+				{img: 'https://x-design.oss-cn-hangzhou.aliyuncs.com/product/info.jpg', name: '', url:''},
+				{img: 'https://x-design.oss-cn-hangzhou.aliyuncs.com/product/info.jpg', name: '', url:''},
 			],
 			commentList:[
 				{username:"XYZ123",
 				scores:4,
-				photo:'/../../static/image/product/椭圆411@3x.png',
+				photo:'https://x-design.oss-cn-hangzhou.aliyuncs.com/product/椭圆411@3x.png',
 				content:"帆布包质量很好，定制的图片也很好看，效果很惊喜！会继续定制的！",
 				photos: [
-					{img: '/static/image/home/蒙版组 8@3x.png', name: '', url:''},
-					{img: '/static/image/home/蒙版组 9@3x.png', name: '', url:''},
-					{img: '/static/image/home/蒙版组 10@3x.png', name: '', url:''},
-					{img: '/static/image/home/蒙版组 11@3x.png', name: '', url:''}
+					{img: 'https://x-design.oss-cn-hangzhou.aliyuncs.com/home/蒙版组 8@3x.png', name: '', url:''},
+					{img: 'https://x-design.oss-cn-hangzhou.aliyuncs.com/home/蒙版组 9@3x.png', name: '', url:''},
+					{img: 'https://x-design.oss-cn-hangzhou.aliyuncs.com/home/蒙版组 10@3x.png', name: '', url:''},
+					{img: 'https://x-design.oss-cn-hangzhou.aliyuncs.com/home/蒙版组 11@3x.png', name: '', url:''}
 				]
 				},
 			],
