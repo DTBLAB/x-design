05ff8ee85fc7e86b4153ce2dc3d329de175c7d95
YingZ000 <18787051147@163.com>
2020-09-19 13:25:49 +0800

    商品部分

--- pages/productlist/productlist.vue
+++ pages/productlist/productlist.vue
@@ -1,7 +1,7 @@
 <template>
 <view class="productlist-container">
 	<view class="productlist__list">
-		<navigator v-for="(item, index) in productlistImages" :key="index" url="../product/product" class="productlist__item">
+		<navigator v-for="(item, index) in productlistImages" :key="index" url="../product/productDesign" class="productlist__item">
 			<image :src="item.img" class="productlist__item__image"></image>
 			<text class="productlist__item__name">{{item.name}}</text>
 		</navigator>
